480cdde7cc3cb634d0b0785edd0e4667b84f8ca6
Farouk314 <fchousein@gmail.com>
2019-05-05 18:48:42 +0100

    Initial navbar in Material-UI

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import AppBar from "@material-ui/core/AppBar";
+import { Menu, Toolbar, Typography, IconButton } from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import MenuItem from "@material-ui/core/MenuItem"
 
 class App extends Component {
+  state = {
+    anchorEl: null
+  }
+
+  handleClick = e => {
+    this.setState({
+      anchorEl: e.currentTarget
+    })
+  }
+
+  handleClose = () => {
+    this.setState({
+      anchorEl: null
+    })
+  }
+
   render() {
+    const { anchorEl } = this.state;
+
     return (
-      <div className="App">
-        <div className="borderTest">
-          TESTING
-        </div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppBar>
+          <Toolbar>
+            <Typography variant="h6" color="inherit">
+              <IconButton color="inherit" aria-haspopup="true" onClick={this.handleClick}>
+                <MenuIcon aria-owns={anchorEl ? "menu" : undefined} />
+              </IconButton>
+              <Menu id="menu" open={Boolean(anchorEl)} onClose={this.handleClose}>
+                <MenuItem onClick={this.handleClose} > Home </MenuItem>
+                <MenuItem onClick={this.handleClose} > About </MenuItem>
+                <MenuItem onClick={this.handleClose} > Contact </MenuItem>
+              </Menu>
+            </Typography>
+          </Toolbar>
+      </AppBar>
     );
   }
 }
